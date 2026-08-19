4f6a1b6fbe591f44f3fa673f1a50888cfb42e01e
Cakemaker8 <madelinepotter8@gmail.com>
2026-08-19 17:24:03 -0500

    ok i should be done now

--- publix.js
+++ publix.js
@@ -17,3 +17,27 @@ async function finalloader(fn) {
 }
 
 finalloader("publix/publixtracker.json")
+
+// https://www.w3schools.com/howto/howto_js_filter_table.asp
+// W3 Schools for the win
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("content");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
